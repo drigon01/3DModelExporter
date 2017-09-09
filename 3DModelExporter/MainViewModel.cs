df3b6acb1c526f844f1c16fc23944217b3bde8aa
drigon <drigon@outlook.com>
2017-09-09 19:49:20 +0200

    initial commit

---
+++ 3DModelExporter/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Windows.Media.Media3D;
+
+namespace _3DModelExporter
+{
+  class MainViewModel : PropertyChangedAware
+  {
+    private OpenFileDialog mDialog = new OpenFileDialog { Title = "Model Browser" };
+
+    private int mNumberOfDetails = 8;
+    public int NumberOfDetails
+    {
+      get { return mNumberOfDetails; }
+      set
+      {
+        mNumberOfDetails = value;
+        NotifyPropertyChanged("NumberOfDetails");
+      }
+    }
+
+    private Visual3D mModel;
+    public Visual3D Model
+    {
+      get { return mModel; }
+      set
+      {
+        mModel = value;
+        NotifyPropertyChanged("Model");
+      }
+    }
+
+    public void OpenModel()
+    {
+      mDialog.ShowDialog();
+      mDialog.FileOk += MDialog_FileOk;
+    }
+
+    private void MDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      mDialog.FileOk -= MDialog_FileOk;
+
+      Model = new FileModelVisual3D { Source = mDialog.FileName };
+    }
+  }
+
+  class PropertyChangedAware : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void NotifyPropertyChanged(string propertyName)
+    {
+      PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+
+  static class Extensions
+  {
+    //public static string NameOf(object property)
+    //{
+    //  return (property.GetType()).GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Where(p => p)
+    //}
+  }
+}
--- 3DModelExporter/MainWindow.xaml.cs
+++ 3DModelExporter/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HelixToolkit.Wpf;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
 
 namespace _3DModelExporter
 {
@@ -23,6 +14,57 @@ namespace _3DModelExporter
     public MainWindow()
     {
       InitializeComponent();
+      DataContext = new MainViewModel();
+      VM.PropertyChanged += VM_PropertyChanged;
+    }
+
+    private void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      mViewPort.Children.Clear();
+      AddContours(VM.Model, VM.NumberOfDetails, VM.NumberOfDetails, VM.NumberOfDetails);
+    }
+
+    private MainViewModel VM { get { return (DataContext as MainViewModel); } }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+      VM.OpenModel();
+    }
+
+    private Plane3D ContourPlane;
+
+
+    private void AddContours(Visual3D model, int o, int m, int n)
+    {
+      var bounds = Visual3DHelper.FindBounds(model, Transform3D.Identity);
+      for (int i = 1; i < n; i++)
+      {
+        this.ContourPlane = new Plane3D(new Point3D(0, 0, bounds.Location.Z + bounds.Size.Z * i / n), new Vector3D(0, 0, 1));
+        Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
+      }
+      for (int i = 1; i < m; i++)
+      {
+        this.ContourPlane = new Plane3D(new Point3D(0, bounds.Location.Y + bounds.Size.Y * i / m, 0), new Vector3D(0, 1, 0));
+        Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
+      }
+      for (int i = 1; i < o; i++)
+      {
+        this.ContourPlane = new Plane3D(new Point3D(bounds.Location.X + bounds.Size.X * i / o, 0, 0), new Vector3D(1, 0, 0));
+        Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
+      }
+    }
+
+    private void AddContours(GeometryModel3D model, Transform3D transform)
+    {
+      var p = ContourPlane.Position;
+      var n = ContourPlane.Normal;
+      var segments = MeshGeometryHelper.GetContourSegments(model.Geometry as MeshGeometry3D, p, n).ToList();
+      foreach (var contour in MeshGeometryHelper.CombineSegments(segments, 1e-6).ToList())
+      {
+        if (contour.Count == 0)
+          continue;
+        mViewPort.Children.Add(new TubeVisual3D { Diameter = 0.03, Path = new Point3DCollection(contour), Fill = Brushes.Green });
+      }
     }
   }
 }
