26737f44e144d96b2804e2f6239a29a79903b33b
drigon <drigon@outlook.com>
2017-09-10 12:00:02 +0200

    add updates

--- 3DModelExporter/MainViewModel.cs
+++ 3DModelExporter/MainViewModel.cs
@@ -1,65 +1,177 @@
 ﻿using HelixToolkit.Wpf;
 using Microsoft.Win32;
 using System.ComponentModel;
+using System.Windows.Input;
 using System.Windows.Media.Media3D;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
 
 namespace _3DModelExporter
 {
-  class MainViewModel : PropertyChangedAware
-  {
-    private OpenFileDialog mDialog = new OpenFileDialog { Title = "Model Browser" };
-
-    private int mNumberOfDetails = 8;
-    public int NumberOfDetails
+    class MainViewModel : PropertyChangedAware
     {
-      get { return mNumberOfDetails; }
-      set
-      {
-        mNumberOfDetails = value;
-        NotifyPropertyChanged("NumberOfDetails");
-      }
+        private OpenFileDialog mDialog = new OpenFileDialog { Title = "Model Browser" };
+
+        private int mNumberOfDetails = 8;
+        public int NumberOfDetails
+        {
+            get { return mNumberOfDetails; }
+            set
+            {
+                mNumberOfDetails = value;
+                NotifyPropertyChanged("NumberOfDetails");
+            }
+        }
+
+        private int mDotsPerInch = 32;
+        public int DotsPerInch
+        {
+            get { return mDotsPerInch; }
+            set
+            {
+                mDotsPerInch = value;
+                NotifyPropertyChanged("DotsPerInch");
+            }
+        }
+
+        private int mLineDiameter = 3;
+        public int LineDiameter
+        {
+            get { return mLineDiameter; }
+            set
+            {
+                mLineDiameter = value;
+                NotifyPropertyChanged("LineDiameter");
+            }
+        }
+
+        private Color mWireframeColor = Colors.Green;
+        public Color WireframeColor
+        {
+            get { return mWireframeColor; }
+            set
+            {
+                mWireframeColor = value;
+                NotifyPropertyChanged("WireframeColor");
+            }
+        }
+
+        private PerspectiveCamera mPerspectiveCamera = new PerspectiveCamera();
+        public PerspectiveCamera PerspectiveCamera { get; set; }
+
+        private Visual3D mModel;// = new new FileModelVisual3D { Source = mDialog.FileName };
+        public Visual3D Model
+        {
+            get { return mModel; }
+            set
+            {
+                mModel = value;
+                NotifyPropertyChanged("Model");
+            }
+        }
+
+        private RelayCommand mPerspectiveSelected;
+        public RelayCommand PerspectiveSelected
+        {
+            get
+            {
+                return mPerspectiveSelected ?? (mPerspectiveSelected =
+                    new RelayCommand((param) =>
+                    {
+                        switch (param.ToString())
+                        {// Need to add proper calc to reposition over model's center
+                            case "Top": PerspectiveCameraSetup(10, -10, 10, -10, 10, -10); break;
+                            //Both should scale with the model to enclose everything in the scene
+                            case "Front": PerspectiveCameraSetup(0, 15, 0, 0, -15, 0); break;
+                            case "Side": PerspectiveCameraSetup(15, 0, 0, -15, 0, 0); break;
+                        }
+                    }, true));
+            }
+        }
+
+        private RelayCommand mExport;
+        public RelayCommand Export
+        {
+            get
+            {
+                return mExport ?? (mExport = new RelayCommand((param) =>
+                {
+                    ExportToPNG((FrameworkElement)param);
+                }, true));
+            }
+        }
+
+        private void ExportToPNG(FrameworkElement viewport)
+        {
+            //viewport.Background = ();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)viewport.ActualWidth, (int)viewport.ActualHeight, DotsPerInch, DotsPerInch, PixelFormats.Pbgra32);
+            bmp.Render(viewport);
+
+            var wPng = new PngBitmapEncoder();
+            wPng.Frames.Add(BitmapFrame.Create(bmp));
+
+            using (Stream wStream = File.Create(@"D:\\" + DateTime.Now.Ticks + ".png")) { wPng.Save(wStream); }
+        }
+
+        public void OpenModel()
+        {
+            mDialog.FileOk += MDialog_FileOk;
+            mDialog.ShowDialog();
+        }
+
+        private void MDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            mDialog.FileOk -= MDialog_FileOk;
+
+            Model = new FileModelVisual3D { Source = mDialog.FileName };
+        }
+
+        public void PerspectiveCameraSetup(double x, double y, double z, double dx, double dy, double dz)
+        {
+            if (PerspectiveCamera == null) PerspectiveCamera = new PerspectiveCamera();
+            PerspectiveCamera.Position = new Point3D(x, y, z);
+            PerspectiveCamera.LookDirection = new Vector3D(dx, dy, dz);
+            PerspectiveCamera.UpDirection = new Vector3D(0, 0, 1);
+            PerspectiveCamera.FieldOfView = 60;
+        }
+
     }
 
-    private Visual3D mModel;
-    public Visual3D Model
+    public class RelayCommand : ICommand
     {
-      get { return mModel; }
-      set
-      {
-        mModel = value;
-        NotifyPropertyChanged("Model");
-      }
+        private Action<object> mAction;
+        private bool mCanExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> action, bool canExecute)
+        {
+            mAction = action;
+            mCanExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) { return mCanExecute; }
+        public void Execute(object parameter) { mAction(parameter); }
     }
 
-    public void OpenModel()
+    class PropertyChangedAware : INotifyPropertyChanged
     {
-      mDialog.ShowDialog();
-      mDialog.FileOk += MDialog_FileOk;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    private void MDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+    static class Extensions
     {
-      mDialog.FileOk -= MDialog_FileOk;
-
-      Model = new FileModelVisual3D { Source = mDialog.FileName };
+        //public static string NameOf(object property)
+        //{
+        //  return (property.GetType()).GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Where(p => p)
+        //}
     }
-  }
-
-  class PropertyChangedAware : INotifyPropertyChanged
-  {
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public void NotifyPropertyChanged(string propertyName)
-    {
-      PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-  }
-
-  static class Extensions
-  {
-    //public static string NameOf(object property)
-    //{
-    //  return (property.GetType()).GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Where(p => p)
-    //}
-  }
 }
--- 3DModelExporter/MainWindow.xaml.cs
+++ 3DModelExporter/MainWindow.xaml.cs
@@ -6,65 +6,71 @@ using System.Windows.Media.Media3D;
 
 namespace _3DModelExporter
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-      DataContext = new MainViewModel();
-      VM.PropertyChanged += VM_PropertyChanged;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+            VM.PropertyChanged += VM_PropertyChanged;
+            VM.PerspectiveCameraSetup(5.3, -12.3, 3.3, -6.3, 11, -6.6);
+            mViewPort.Camera = VM.PerspectiveCamera;
+        }
+
+        private void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            mViewPort.Children.Clear();
+            AddContours(VM.Model, VM.NumberOfDetails, VM.NumberOfDetails, VM.NumberOfDetails);
+        }
+
+        private MainViewModel VM { get { return (DataContext as MainViewModel); } }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            VM.OpenModel();
+        }
+
+        private Plane3D ContourPlane;
+
+        private void AddContours(Visual3D model, int o, int m, int n)
+        {
+            var bounds = Visual3DHelper.FindBounds(model, Transform3D.Identity);
+            for (int i = 1; i < n; i++)
+            {
+                this.ContourPlane = new Plane3D(new Point3D(0, 0, bounds.Location.Z + bounds.Size.Z * i / n), new Vector3D(0, 0, 1));
+                Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
+            }
+            for (int i = 1; i < m; i++)
+            {
+                this.ContourPlane = new Plane3D(new Point3D(0, bounds.Location.Y + bounds.Size.Y * i / m, 0), new Vector3D(0, 1, 0));
+                Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
+            }
+            for (int i = 1; i < o; i++)
+            {
+                this.ContourPlane = new Plane3D(new Point3D(bounds.Location.X + bounds.Size.X * i / o, 0, 0), new Vector3D(1, 0, 0));
+                Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
+            }
+        }
+
+        private void AddContours(GeometryModel3D model, Transform3D transform)
+        {
+            var wPosition = ContourPlane.Position;
+            var wNormal = ContourPlane.Normal;
+            var wSegments = MeshGeometryHelper.GetContourSegments(model.Geometry as MeshGeometry3D, wPosition, wNormal).ToList();
+            foreach (var wContour in MeshGeometryHelper.CombineSegments(wSegments, 1e-6).ToList())
+            {
+                if (wContour.Count == 0)
+                    continue;
+                mViewPort.Children.Add(new TubeVisual3D
+                {
+                    Diameter = 0.03,
+                    Path = new Point3DCollection(wContour),
+                    Fill = new SolidColorBrush(VM.WireframeColor)
+                });
+            }
+        }
     }
-
-    private void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-      mViewPort.Children.Clear();
-      AddContours(VM.Model, VM.NumberOfDetails, VM.NumberOfDetails, VM.NumberOfDetails);
-    }
-
-    private MainViewModel VM { get { return (DataContext as MainViewModel); } }
-
-    private void OnClick(object sender, RoutedEventArgs e)
-    {
-      VM.OpenModel();
-    }
-
-    private Plane3D ContourPlane;
-
-
-    private void AddContours(Visual3D model, int o, int m, int n)
-    {
-      var bounds = Visual3DHelper.FindBounds(model, Transform3D.Identity);
-      for (int i = 1; i < n; i++)
-      {
-        this.ContourPlane = new Plane3D(new Point3D(0, 0, bounds.Location.Z + bounds.Size.Z * i / n), new Vector3D(0, 0, 1));
-        Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
-      }
-      for (int i = 1; i < m; i++)
-      {
-        this.ContourPlane = new Plane3D(new Point3D(0, bounds.Location.Y + bounds.Size.Y * i / m, 0), new Vector3D(0, 1, 0));
-        Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
-      }
-      for (int i = 1; i < o; i++)
-      {
-        this.ContourPlane = new Plane3D(new Point3D(bounds.Location.X + bounds.Size.X * i / o, 0, 0), new Vector3D(1, 0, 0));
-        Visual3DHelper.Traverse<GeometryModel3D>(model, this.AddContours);
-      }
-    }
-
-    private void AddContours(GeometryModel3D model, Transform3D transform)
-    {
-      var p = ContourPlane.Position;
-      var n = ContourPlane.Normal;
-      var segments = MeshGeometryHelper.GetContourSegments(model.Geometry as MeshGeometry3D, p, n).ToList();
-      foreach (var contour in MeshGeometryHelper.CombineSegments(segments, 1e-6).ToList())
-      {
-        if (contour.Count == 0)
-          continue;
-        mViewPort.Children.Add(new TubeVisual3D { Diameter = 0.03, Path = new Point3DCollection(contour), Fill = Brushes.Green });
-      }
-    }
-  }
 }
